dce198b32312b1f86a1f70471e3a9be123c7ec7a
Alfred Wang <alfredwang0707@gmail.com>
2020-10-15 19:58:46 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 require "pry"
 def reverse_each_word(string)
   
-  reversed = string.split(" ").each do |word|
+  reversed = string.split(" ").map! |word|
   word.reverse
   end
 reversed.join(" ")
