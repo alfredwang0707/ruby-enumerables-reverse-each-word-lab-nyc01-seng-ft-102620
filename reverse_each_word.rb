b1698f45bc0d2f43f812fd8d2bcc99f80a908a9c
Alfred Wang <alfredwang0707@gmail.com>
2020-10-15 20:04:07 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,13 @@
 require "pry"
 def reverse_each_word(string)
+  #use split to make it into array so we can enumerate
+  #assign the array,use reverse, and then join them back
 reversed = string.split(" ").each do |word|
   word.reverse
  end
  reversed.join(" ")
 end
+
+#collect → an_enumerator
+#Returns a new array with the results of running block once for every element in enum.
+# each 
