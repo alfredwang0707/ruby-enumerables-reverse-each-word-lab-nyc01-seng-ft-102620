9f4ae464de5f0c7ce6e90982e9242f82bccaecaa
Alfred Wang <alfredwang0707@gmail.com>
2020-10-15 20:26:09 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,6 +2,16 @@ require "pry"
 #collect → Returns a new array with the results of running block once for every element in enum.
 
 # each  Calls block once for each element in self, passing that element as a parameter, converting multiple values from yield to an array.
+def reverse_each_word_with_each(string)
+ 
+ array = string.split(" ")
+  return_array = []
+  array.each do|string|
+    return_array << string.reverse
+  end
+  return_array.join(" ")
+end
+
 
 def reverse_each_word(string)
   #use split to make it into array so we can enumerate
@@ -12,13 +22,5 @@ reversed = string.split(" ").collect do |word|
  reversed.join(" ")
 end
 
-def reverse_each_word_with_each(string)
- 
- array = string.split(" ")
-  return_array = []
-  array.each do|string|
-    return_array << string.reverse
-  end
-  return_array.join(" ")
-end
+
 
